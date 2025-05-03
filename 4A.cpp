bd03af3b22b1f916900d63bde4cfda0fad0d664b
jesan787 <120408210+jesan787@users.noreply.github.com>
2025-05-03 13:18:00 +0600

    Create 4A.cpp

---
+++ 4A.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+    using namespace std;
+    int main(){
+        string S,T;
+        cin>> S>>T;
+        cout<< S.size()<< " " << T.size()<<endl;
+        cout<< S<< " " << T<<endl;
+    }
