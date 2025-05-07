36024950393d480e1fe7d1b8bc4f496ac309d13e
jesan787 <120408210+jesan787@users.noreply.github.com>
2025-05-07 15:08:05 +0600

    Update 4g.cpp

--- 4g.cpp
+++ 4g.cpp
@@ -1,20 +1,46 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main()
-{
-    string S;
-    cin >> S;
+// int main()
+// {
+//     string S;
+//     cin >> S;
 
-    for (char &c : S) {
-        if (c == ',')
-            c = ' ';
-        else if (islower(c))
-            c = toupper(c);
-        else if (isupper(c))
-            c = tolower(c);
+//     for (char &c : S) {
+//         if (c == ',')
+//             c = ' ';
+//         else if (islower(c))
+//             c = toupper(c);
+//         else if (isupper(c))
+//             c = tolower(c);
+//     }
+
+//     cout << S << endl;
+//     return 0;
+// }
+
+
+    #include<iostream>
+    #include<string>
+    using namespace std;
+    int main()
+    {
+        string s;
+      getline(cin,s);
+     
+        for(int i=0; i<s.size(); i++)
+        {
+            if( s[i]==',')
+            cout<<" ";
+            if( islower(s[i])){
+               s[i]=toupper(s[i]);
+            cout<<s[i];//toupper(s[i]);
+            }
+     
+            else if( isupper(s[i])){
+            s[i]=tolower(s[i]);
+            cout<<s[i];//tolower(s[i])<<endl;
+            }
+     
+        }
     }
-
-    cout << S << endl;
-    return 0;
-}
