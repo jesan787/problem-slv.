30c6f3bba2d657677f3b60330c8751af1c3f8479
jesan787 <120408210+jesan787@users.noreply.github.com>
2026-04-30 13:59:14 +0600

    Implement password check in Fixed_Password.c

---
+++ Fixed_Password.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    
+    
+      for(int i=1;i>0;i++){
+        scanf("%d",&x);
+        if(x==1999){
+        printf("Correct\n");
+        break;}
+        
+        else
+        {
+            printf("Wrong\n");
+        }
+
+    }
+ 
+    return 0;
+}
